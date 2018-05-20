7d537d26d48cec196614441baf5dc693aaffc002
Lucian Gheorghe <lucian.gheorghe1989@gmail.com>
2018-05-20 13:24:47 +0300

    products added to app

--- shop/models.py
+++ shop/models.py
@@ -14,8 +14,8 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    # def get_absolute_url(self):
-        # return reverse('shop:product_list_by_category', args=[self.slug])
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
 
 
 class Product(models.Model):
@@ -37,5 +37,5 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-    # def get_absolute_url(self):
-    #     return reverse('shop:product_detail', args=[self.id, self.slug])
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])

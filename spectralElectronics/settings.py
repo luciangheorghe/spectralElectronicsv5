ccdf4edd39e23735d956a614a2be0a4fb1663157
Lucian Gheorghe <lucian.gheorghe1989@gmail.com>
2018-05-26 14:59:37 +0300

    add allowed host

--- spectralElectronics/settings.py
+++ spectralElectronics/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['f2e26066.ngrok.io', '127.0.0.1']
+ALLOWED_HOSTS = ['f2e26066.ngrok.io', '127.0.0.1', 'spectralelectronics2.herokuapp.com']
 
 
 # Application definition
